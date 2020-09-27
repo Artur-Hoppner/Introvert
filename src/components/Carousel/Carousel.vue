7524b3ae0aa6ff83be8420fa4c1ecdd9b80718ec
Artur <Artur.Hoppner@hotmail.com>
2020-09-27 12:23:11 +0200

    fixed like button

---
+++ src/components/Carousel/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-carousel
+    interval="4000"
+    cycle
+    height="50"
+    :show-arrows="false"
+    hide-delimiters
+  >
+    <v-carousel-item
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+      <v-sheet color="white" height="100%">
+        <v-row align="end">
+          <div class="display-1 text-color">Creative</div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+      <v-sheet color="white" height="100%">
+        <v-row>
+          <div class="display-1 text-color">Inovation</div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.text-color {
+  color: #ff969e;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+</style>
